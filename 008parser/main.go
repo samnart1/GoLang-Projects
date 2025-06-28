2f11d521ea6ce0fa273276c12ee70dd5b0185216
samnart1 <originalsamnart@gmail.com>
2025-06-28 05:21:37 +0200

    add test data

--- 008parser/main.go
+++ 008parser/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/samnart1/golang/008parser/cmd"
+)
+
+func main() {
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
